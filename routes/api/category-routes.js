9aa9b85b7fd2d6a1dd21a6c904c62ea957c902b8
leviathan <jake66martin@hotmail.com>
2023-11-07 18:29:44 -0500

    added code for get by id on category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,9 +18,21 @@ const { Category, Product } = require('../../models');
   });
 // });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'There is no category by that id.' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
